156cd6a378f907673ecac7a3810f4e895164fd83
Eugene <eugeneayonga@gmail.com>
2022-09-04 13:43:40 +0300

    Completed Assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
+# class CashRegister
+# .new
+# takes one optional argument, an employee discount, on initialization
+# sets an instance variable @total on initialization to zero
+class CashRegister
+
+    attr_accessor :total, :discount, :items, :last_transaction
+
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+
+    def add_item(title, price, quantity = 1)
+        self.total += price * quantity
+        quantity.times do
+        items << title
+        end
+        self.last_transaction = price * quantity
+    end
+
+
+    def apply_discount
+        if discount != 0
+        self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+        "After the discount, the total comes to $#{self.total}."
+        else
+        "There is no discount to apply."
+        end
+    end
+
+    
+    def void_last_transaction
+        self.total = self.total - self.last_transaction
+    end
+
+end
+    
 
